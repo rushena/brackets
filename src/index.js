b468509c01271f870eab983a6156c74d4ca5f6ad
RushenA <rushena@MacBook-Air-RushenA.local>
2021-03-15 00:47:55 +0500

    Write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const strToArr = str.split('');
+	const leftBrackets = [];
+	const rightBrackets = [];
+	const sameBrackets = [];
+	let checkStr = '';
+	
+	bracketsConfig.forEach(([left, right]) => {
+		if (left === right) {
+			sameBrackets.push(left)
+		} else {
+			leftBrackets.push(left);
+			rightBrackets.push(right);
+		}
+	});
+
+	strToArr.forEach(item => {
+		if (checkStr === null) return;
+
+		if (leftBrackets.includes(item)) {
+			checkStr += item;
+		} else if (rightBrackets.includes(item)) {
+			if (checkStr.slice(-1) === leftBrackets[rightBrackets.indexOf(item)]) {
+				checkStr = checkStr.slice(0, -1);
+			} else {
+				checkStr = null
+			}
+		} else if (checkStr.includes(item)) {
+			if (checkStr.slice(-1) === item) {
+				checkStr = checkStr.slice(0, -1);
+			} else {
+				checkStr = null
+			}
+		} else {
+			checkStr += item;
+		}
+	});
+
+	return checkStr === "";
 }
